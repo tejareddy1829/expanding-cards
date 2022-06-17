ccb08c43d750bad8daf0b1222aefc7bf1850a061
tejareddy1829 <tejar9700565670@gmail.com>
2022-06-17 13:30:58 +0200

    js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-'use strict';
-const panel= document.querySelectorAll('.panel')
+"use strict";
+const panel = document.querySelectorAll(".panel");
 
-panel.forEach(panel => {
-    
-})
+panel.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panel.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
